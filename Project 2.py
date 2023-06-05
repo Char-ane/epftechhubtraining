949ccc0f436f35302cdbc6e73a72c0f58a83c9dc
Char-ane <100921479+Char-ane@users.noreply.github.com>
2023-06-05 08:25:26 +0200

    Update Project 2.py

--- Project 2.py
+++ Project 2.py
@@ -1,5 +1,13 @@
-sum=0
-for i in range(1000):
-    if i%3==0 or i%5==0:
-        sum+=i
-print(sum)
+total=1
+num1=1
+num2=0
+fnum=0
+
+while fnum<4000000:
+    num1=num2
+    num2=total
+    total=num1+num2
+
+    if total%2==0:
+        fnum+=total
+print(fnum)
